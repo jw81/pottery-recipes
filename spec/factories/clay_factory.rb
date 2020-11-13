93c2052cab8b94f50970bea8bc9682d018d00f4e
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2020-11-13 16:04:16 -0600

    Adds the Clay model
    - also fixes up some typos in other specs

---
+++ spec/factories/clay_factory.rb
@@ -0,0 +1,8 @@
+# Represent the Clay model
+FactoryBot.define do
+  factory :clay do
+    name { 'My Clay Name' }
+    description { 'My Clay Description' }
+    brand_name { 'My Clay Brand Name' }
+  end
+end
