8aeef6ec60d957ba3a7fef28c1d03d868e5886e1
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2020-12-04 16:01:58 -0600

    Adds logic to seed DB with Amaco glazes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Storing Amaco Glazes in database...'
+GlazeStructure = Struct.new(:brand_name, :name)
+entries = CSV.read(File.join(Rails.root, 'tmp/scrapes/output/amaco.csv'))
+entries = entries.map { |entry| GlazeStructure.new(entry[0], entry[1]) }
+entries.each { |entry| Glaze.create!(brand_name: entry.brand_name, name: entry.name) }
+puts 'Storing Amaco Glazes in database...DONE'
